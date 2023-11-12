9219d717f445d020eb34b5693ef2fd0d6b02815e
AndrewsTDA <AndrewsTDA@outlook.com>
2023-11-12 19:42:07 +0000

    general upgrades

--- src/rgg.py
+++ src/rgg.py
@@ -3,97 +3,183 @@ import matplotlib.pyplot as plt
 import networkx as nx
 from itertools import combinations
 import matplotlib
+from dataclasses import dataclass
 
-matplotlib.use('TkAgg')
+matplotlib.use("TkAgg")
+
+
+@dataclass
+class Node:
+    node: int
+    position: np.ndarray
+
+
+@dataclass
+class Order:
+    node: int
+    height: int
+    depth: int
 
 
 class Graph:
     def __init__(self, n, radius, d):
-        self.n = n  # number of nodes
-        self.d = d  # number of spacetime dimensions
-        self.radius = radius  # radius of connection
+        """
+        Instantiate a time-ordered list of nodes as well as other information
+        required to generate an RGG graph in minkowski space time
 
-        # ledger of edges in tuple pairs. format: (from node, to node)
+        Args:
+            n: Number of nodes
+            radius: Proper time radius that defines threshold of node connection
+            d: Number of dimensions (including time)
+        """
+        self.n = n
+        self.d = d
+        self.radius = radius
         self.edges = []
-        # ledger of connections going out from a node
-        self.adjacency = [[] for _ in range(self.n)]
-
-        # Generate positions of nodes and sort them topologically
-        poses = [np.random.uniform(0, 1, d) for _ in range(self.n)]
-        poses = sorted(poses, key=lambda pos: pos[0])  # top sort by time
-        # positions of nodes [(node number, np.array of positions), (...), ...]
-        self.nodes = [(node, pos) for node, pos in enumerate(poses)]
+        self.longest_path = []
+        self.children = [[] for _ in range(self.n)]
+        self.parents = [[] for _ in range(self.n)]
+        positions = [np.random.uniform(0, 1, d) for _ in range(self.n)]
+        positions = sorted(positions, key=lambda pos: pos[0])
+        self.nodes = [Node(node, pos) for node, pos in enumerate(positions)]
+        self.height_and_depth = [Order(node, 0, 0) for node in range(self.n)]
 
     @staticmethod
     def find_max_list(lst):
-        # finds longest list in a list of lists and returns it
+        """
+        Finds longest element in list of lists
+        Args:
+            lst: some iterable variable, list, set, etc
+        """
         max_list = max(lst, key=lambda i: len(i))
         return max_list
 
-    def make_edges_minkowski(self):  # connect nodes fitting conditions
-        # check all possible combinations of two nodes
-        for (node1, pos1), (node2, pos2) in combinations(self.nodes, 2):
-            dt2 = (pos2[0] - pos1[0]) ** 2  # time separation squared
-            # space separation squared
-            dx2 = sum([(pos2[i] - pos1[i]) ** 2 for i in range(1, self.d)])
-            interval = -dt2 + dx2  # proper time squared, signature -1, 1, ...
+    def node_position(self, index):
+        """
+        Find the position of a node given the index
+        """
+        return self.nodes[index].position
 
-            # connect within certain interval (timelike or null like + within R)
-            if - self.radius * self.radius < interval < 0:
-                # connect forward in time only (forward pointing)
-                edge = (node1, node2) if pos1[0] < pos2[0] else (node2, node1)
+    @property
+    def minkowski_metric(self):
+        identity = np.identity(n=self.d)
+        identity[0][0] *= -1
+        return identity
+
+    def make_edges_minkowski(self):
+        """
+        Generate edges if two nodes are within self.radius of each other
+        and are time-like separated
+        """
+        for node1, node2 in combinations(self.nodes, 2):
+            interval = self.interval((node1, node2))
+
+            if -self.radius * self.radius < interval < 0:
+                edge = (
+                    (node1.node, node2.node)
+                    if node1.position[0] < node2.position[0]
+                    else (node2.node, node1.node)
+                )
                 self.edges.append(edge)
-                self.adjacency[edge[0]].append(edge[1])
+                self.children[edge[0]].append(edge[1])
+                self.parents[edge[1]].append(edge[0])
 
-    def dist(self, node0, node1):
-        # positions of the nodes
-        pos0 = self.nodes[node0][1].copy()
-        pos1 = self.nodes[node1][1].copy()
+    def interval(self, node_pair):
+        """
+        ds^2 between two nodes
+        Args:
+            node_pair: either the index or the node itself
+        """
+        if all([isinstance(node, int) for node in node_pair]):
+            pos0 = self.node_position(node_pair[0])
+            pos1 = self.node_position(node_pair[1])
+        elif all([isinstance(node, Node) for node in node_pair]):
+            pos0 = node_pair[0].position
+            pos1 = node_pair[1].position
+        else:
+            raise AttributeError("wrong format of node passed to distance function")
+        dx = pos1 - pos0
 
-        pos0[0] = -pos0[0]  # minkowski metric. signature -1, 1, ...
+        return self.minkowski_metric @ dx @ dx
 
-        return np.dot(pos0, pos1)
+    def proper_time(self, node_pair):
+        return np.sqrt(-self.interval(node_pair))
 
-    def longest_path(self):
-
-        # Find pairs of nodes that make the longest path
-
-        chains = [set() for _ in range(self.n)]  # pairs in longest path
-        vis = [False] * self.n  # if node has been visited
-        for node in range(self.n):
-            if not vis[node]:  # dynamic: don't look at visited nodes
-                self.longest_path_per_node(node, chains, vis)
-
-        max_chain = self.find_max_list(chains)
-
-        # Find distance covered by longest path
+    def geometric_length(self, path=None):
+        """
+        Find the geometric length of a path
+        Args:
+            path: a list of tuples of node pairs forming the
+            edges in this path
+        """
+        if path is None:
+            path = self.longest_path
 
         tot_distance = 0
-        for edge_pair in max_chain:
-            tot_distance += self.dist(edge_pair[0], edge_pair[1])
+        for node_pair in path:
+            tot_distance += self.proper_time(node_pair)
 
-        return {'Longest path edge pairs': max_chain,
-                'Longest path proper time': tot_distance}
+        return tot_distance
+
+    def find_longest_path(self):
+        """
+        Find a longest path in the network. Note that this will return
+        only one possible longest path. The nature of which is chosen
+        is decided by python's max function.
+        """
+        chains = [set() for _ in range(self.n)]
+        vis = [False] * self.n
+        for node in range(self.n):
+            if not vis[node]:
+                self.longest_path_per_node(node, chains, vis)
+
+        self.longest_path = list(self.find_max_list(chains))
 
     def longest_path_per_node(self, node, chains, vis):
+        """
+        iterate through all nested children, considering them
+        only if they have not been previously visited. Choose longest
+        between current longest path to node and path to current
+        child plus edge from child to node.
+
+        Args:
+            node: the parent node we are considering
+            chains: the longest known chains of all nodes
+            vis: which nodes have already been visited
+        """
         vis[node] = True
-        for child in self.adjacency[node]:
-            # find the longest chains of all children
+
+        for child in self.children[node]:
             if not vis[child]:
                 self.longest_path_per_node(child, chains, vis)
 
-            # select child with longest chains (if same then picks path)
             chains[node] = self.find_max_list(
-                [chains[node], {(node, child)}.union(chains[child])])
+                [chains[node], {(node, child)}.union(chains[child])]
+            )
+            self.height_and_depth[node].height = len(chains[node])
+
+    def angular_deviation(self, path=None):
+        """
+        Find the total angular deviation along a path
+        Args:
+            path: a list of edges included in the path
+        """
+        if path is None:
+            path = self.longest_path
+        ordered_path = sorted(path, key=lambda edge: edge[0])
+        angle_index_combos = [(i, j) for i, j in combinations([_ for _ in range(self.d)], 2)]
+        angles = []
 
 
-if __name__ == '__main__':
-    n = 15
+if __name__ == "__main__":
+    n = 10
     graph = Graph(n, 0.3, 2)
     graph.make_edges_minkowski()
-    print(graph.longest_path())
+    graph.find_longest_path()
+    print(graph.geometric_length())
     g = nx.DiGraph()
     g.add_nodes_from(range(n))
     g.add_edges_from(graph.edges)
-    nx.draw(g, [(n[1][1], n[1][0]) for n in graph.nodes], with_labels=True)
+    # print([graph.height_and_depth[i].height for i in range(n)])
+    nx.draw(g, [(n.position[1], n.position[0]) for n in graph.nodes], with_labels=True)
     plt.show()
