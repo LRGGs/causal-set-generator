e8fdfbda1df2120d0f16c12c6fdc8fadabdfadae
Thomas Andrews <andrewstda@outlook.com>
2023-11-29 15:10:49 +0000

    mean sep for n orders

--- src/analysis.py
+++ src/analysis.py
@@ -1,5 +1,7 @@
 import pickle
 from src.rgg import Node, Order, Paths
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def read_pickle(n, r, d, i):
@@ -10,5 +12,27 @@ def read_pickle(n, r, d, i):
         return file
 
 
+def mean_distance_by_order(order_collections, orders=10):
+    means = []
+    for graph in order_collections:
+        order_means = []
+        for i in range(orders):
+            order_means.append(np.sqrt(np.mean([node[1]**2 for node in graph[i]])))
+        means.append(order_means)
+    total_means = np.mean(means, axis=0)
+    total_stdvs = np.std(means, axis=0)
+    plt.errorbar([i for i in range(orders)], total_means, yerr=total_stdvs, ls='none', capsize=5, marker="x")
+    plt.title(f"Mean Separation from Geodesic for the First {orders} Orders")
+    plt.xlabel("Order")
+    plt.ylabel("Mean Separation")
+
+    plt.show()
+
+
+def mean_distance_by_path(graphs, orders=10):
+    pass
+
+
 if __name__ == '__main__':
-    read_pickle(10000, 1, 2, 100)
+    graphs = read_pickle(10000, 1, 2, 100)
+    mean_distance_by_order([graph["order_collections"] for graph in graphs])
