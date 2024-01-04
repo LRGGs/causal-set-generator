e4b6ac51a950d9471346d2683a98b2c3dd00bb41
AndrewsTDA <AndrewsTDA@outlook.com>
2024-01-04 16:02:06 +0000

    poetry cleanup

--- src/rgg.py
+++ src/rgg.py
@@ -535,7 +535,7 @@ def main():
     # cProfile.run("run(20, 1, 2, i=1, p=False, m=False, g=False)", "profiler")
     # pstats.Stats("profiler").strip_dirs().sort_stats("tottime").print_stats()
 
-    multi_run(list(np.linspace(1000, 10000, 100, dtype=int)), 0.5, 2, 1)
+    multi_run(list(np.linspace(1000, 10000, 100, dtype=int)), 0.1, 2, 1)
 
     # multi_run(20, 0.5, 2, 10)
 
