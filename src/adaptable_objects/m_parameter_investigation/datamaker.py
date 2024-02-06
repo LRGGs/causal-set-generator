2c87bab002e0382c4daad6aa516cc37cca985b1b
Maxim Asmat <maximasmat7@gmail.com>
2024-02-06 21:28:23 +0000

    transfer to computer not many changes

--- src/adaptable_objects/m_parameter_investigation/datamaker.py
+++ src/adaptable_objects/m_parameter_investigation/datamaker.py
@@ -13,20 +13,20 @@ def run(n):  # Generating dataframe of one network
     return path_length
 
 if __name__ == '__main__':
-    multiprocessing.set_start_method("spawn")
+    multiprocessing.set_start_method("forkserver")
 
     n_experiments = 10  # number of times we measure with the same parameters
-    n_range = [n for n in range(100, 1001, 100)]
+    n_range = [n for n in range(100, 15001, 100)]
     r = 2
     d = 2
 
     for experiment in tqdm(range(n_experiments)):
         # Run in parallel for different inputs
 
-        cpus = multiprocessing.cpu_count() - 1
+        cpus = multiprocessing.cpu_count() - 5
         p = multiprocessing.Pool(processes=cpus)
 
-        results = p.map(run, n_range)  # multiprocess different n
+        results = p.map(run, n_range, 10)  # multiprocess different n
         print(results)
 
         with open(file_namer(n_range, r, d, experiment), "wb") as fp:
